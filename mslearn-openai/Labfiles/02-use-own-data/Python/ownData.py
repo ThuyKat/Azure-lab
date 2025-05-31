921485458aaa824011bc544ecccd5d17132bb9e0
Thuy Katie Nguyen <katiefirst7@gmail.com>
2025-06-01 01:23:17 +1000

    add completed lab for rag

--- mslearn-openai/Labfiles/02-use-own-data/Python/ownData.py
+++ mslearn-openai/Labfiles/02-use-own-data/Python/ownData.py
@@ -15,7 +15,33 @@ client = openai.AzureOpenAI(
     api_version="2024-02-01",
 )
 
-# Configure your data source
+ # Configure your data source
+text = input('\nEnter a question:\n')
+
+completion = client.chat.completions.create(
+    model=deployment,
+    messages=[
+        {
+            "role": "user",
+            "content": text,
+        },
+    ],
+    extra_body={
+        "data_sources":[
+            {
+                "type": "azure_search",
+                "parameters": {
+                    "endpoint": os.environ["AZURE_SEARCH_ENDPOINT"],
+                    "index_name": os.environ["AZURE_SEARCH_INDEX"],
+                    "authentication": {
+                        "type": "api_key",
+                        "key": os.environ["AZURE_SEARCH_KEY"],
+                    }
+                }
+            }
+        ],
+    }
+)
 
 
 print(completion.model_dump_json(indent=2))
